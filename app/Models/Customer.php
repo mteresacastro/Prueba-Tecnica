2cbbd8ee7cbb26eb334e556b619d1d6e5be180f5
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-10-09 10:49:08 +0200

    añado método boot a customerController para que el softdelete se refleje en la tabla customers:hobbies

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Customer extends Model
 {
@@ -22,5 +23,19 @@ class Customer extends Model
     {
         return $this->belongsToMany(Hobby::class, 'customers_hobbies')->withTimestamps();
     }
+
+
+    //Método para que al borrar un customer añada el softDelete a la tabla customers_hobbies.Explicación:
+    protected static function boot() //boot es un método especial en los modelos Eloquent. Este método se ejecuta cuando el modelo se inicializa.
+    {
+        parent::boot(); //Llama al método boot de la clase padre (Model)
+
+        static::deleting(function ($customer)/*Define un evento deleting que se ejecuta justo antes de que un registro Customer sea eliminado. La función anónima recibe el objeto $customer que está siendo eliminado.*/ {
+            DB::table('customers_hobbies') //Selecciona la tabla customers_hobbies usando el facade DB
+                ->where('customer_id', $customer->id)//Filtra los registros donde customer_id coincide con el ID del customer que está siendo eliminado.
+                ->update(['deleted_at' => now()]);//Actualiza la columna deleted_at con la fecha y hora actuales (now()) para marcar los registros como eliminados (soft delete).
+        });
+    }
 }
+
 
